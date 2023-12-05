05a4a7a3f51d77c1f71a7ebf3a4c7b593d42e411
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-05 14:44:26 +0700

    Add files via upload

---
+++ Exercise/bai_2/constants.h
@@ -0,0 +1,21 @@
+// constants.h of "Tính tiền đi Taxi"
+#ifndef _CONSTANTS_
+#define _CONSTANTS_
+
+#include <iostream>
+using namespace std;
+
+
+// Định nghĩa namespace chứa các hằng số
+namespace constants
+{
+	const int nguong_1(1);
+	const int nguong_2(5);
+	const int nguong_3(120);
+    const int gia_1(15000);
+    const int gia_2(13500);
+    const int gia_3(11000);
+    const int phantram(0.1);
+	
+}
+#endif // !_CONSTANTS_
