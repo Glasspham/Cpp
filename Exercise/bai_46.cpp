94d5bc552b61ba6c38b1c84c45e3ec82813b9502
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-12 15:07:30 +0700

    Add files via upload

--- Exercise/bai_46.cpp
+++ Exercise/bai_46.cpp
@@ -19,50 +19,32 @@ int main()
         cout << arr[i] << ' ';
     cout << endl;
 
-    // Tìm giá trị min trong mảng
-    int value_min = arr[0];
+    // Tìm vị trí và giá trị âm đầu tiên trong mang
+    int index_first;
+    int value_first;
     for(int i = 0; i < nSize; i++)
-    {
-        if(arr[i] < value_min)
+        if(arr[i] < 0)
         {
-            value_min = arr[i];
+            index_first = i;
+            value_first= arr[i];
+            break;
         }
-    }
 
-    // Xét xem giá trị nào lớn hơn min
-    int index_max = -1; // Khởi tạo index_max bằng -1
-    int value_max = -101; // Khởi tạo value_max bằng một giá trị thấp hơn giá trị âm nhỏ nhất có thể có
-    for(int i = 0; i < nSize; i++)
-    {
+    // Tìm vị trí giá trị âm lớn nhất
+    int index_max = -1;
+    int value_max = value_first;
+    for(int i = index_first; i < nSize; i++)
         if(arr[i] < 0 && arr[i] > value_max)
         {
             value_max = arr[i];
             index_max = i;
         }
-    }
   
     if(index_max == -1) 
-    {
         cout << "Khong co so am trong mang." << endl;
-    } 
     else
-    {
         cout << "Vi tri phan tu am lon nhat trong mang: " << index_max << '\n';
-    }
   
     system("pause");
     return 0;
 }
-/*
-Lưu ý đoạn code này không hoàn toàn hoàn hảo vì thực tế không cần lệnh ở "Tìm giá trị min trong mảng".
-Mà sẽ xét giá trị index_max = -1 và value_max = -101, thì cái -101 là số lớn hơn giá trị mà random ra được
-do cái dòng rand() % 100 là random số trong khoảng 100. Nếu giới hạn cao hơn thì + 1 đơn vị.
-Còn không thì dùng "Tìm giá trị min trong mảng" thì khai báo lại như sau:
-int index_max = -1;
-int value_max = value_min;
-Nhưng nó sẽ bị lỗi khi gặp trường hợp 1 âm còn lại dương:
-vd arr = {1,2,3,-4,5}
-Nên mới có cách trên lấy hẳn 1 con nhỏ nhất.
-Nên lời khuyên tốt nhất hãy tự viết 1 bài khác hoặc thêm khảo trên How Kteam ở những bài tập cơ bản C++
-https://howkteam.vn/course/bai-toan-kinh-dien-trong-lap-trinh/viet-ham-tim-vi-tri-cua-phan-tu-lon-nhat-trong-mang-cac-so-nguyen-1473
-*/
