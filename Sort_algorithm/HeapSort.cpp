82722f4fcd88aebe3cf8cd77b3b5d64fb116ea94
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-27 22:06:00 +0700

    Update HeapSort.cpp

--- Sort_algorithm/HeapSort.cpp
+++ Sort_algorithm/HeapSort.cpp
@@ -4,11 +4,16 @@ using namespace std;
   
 void heap(int a[], int n, int i)
 {
-    int root = i, left = 2 * i + 1, right = 2 * i + 2;
+    int root = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
     if(left < n && a[left] > a[root])
         root = left;
+  
     if(right < n && a[right] > a[root])
         root = right;
+  
     if(root != i)
     {
         swap(a[i], a[root]);
@@ -20,6 +25,7 @@ void heapSort(int a[], int n)
 {
     for(int i = n / 2 - 1; i >= 0; i--)
         heap(a, n, i);
+  
     for(int i = n - 1; i > 0; i--)
     {
         swap(a[0], a[i]);
