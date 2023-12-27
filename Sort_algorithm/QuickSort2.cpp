479ff496443ae83b0ef1d86b30702c578c9610ff
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-27 22:13:07 +0700

    Update QuickSort2.cpp

--- Sort_algorithm/QuickSort2.cpp
+++ Sort_algorithm/QuickSort2.cpp
@@ -6,7 +6,6 @@ int partition(int a[], int l, int r)
 {  
     int j = l - 1, pivot = a[r];
     for(int i = l; i < r; i++)
-    {
         if(a[i] <= pivot)
         {
             ++j;
@@ -14,7 +13,6 @@ int partition(int a[], int l, int r)
             a[i] = a[j];
             a[j] = tmp;
         }
-    }
     j++;
     int tmp = a[r];
     a[r] = a[j];
@@ -35,10 +33,8 @@ void quickSort(int a[], int l, int r)
 int main()  
 {  
     int a[] = {10, 5, 1, 6, 2, 7, 8, 3, 4, 9};  
-    int n = sizeof(a)/sizeof(a[0]);  
+    int n = sizeof(a) / sizeof(a[0]);  
     quickSort(a, 0, 9);  
     for(int i = 0; i < n; i++)
-    {
         cout << a[i] << ' ';
-    }  
 }  
