f1aa9c55baf0b70d568047b130f5a14940a1da14
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-27 22:16:28 +0700

    Update SelectionSort.cpp

--- Sort_algorithm/SelectionSort.cpp
+++ Sort_algorithm/SelectionSort.cpp
@@ -4,11 +4,11 @@ using namespace std;
   
 void selectionSort(int a[], int n)  
 {  
-    for (int i = 0; i < n-1; i++)  
+    for (int i = 0; i < n - 1; i++)  
     {  
         int min_idx = i;  
       
-        for (int j = i+1; j < n; j++)  
+        for (int j = i + 1; j < n; j++)  
           if (a[j] < a[min_idx])  
               min_idx = j;
 
