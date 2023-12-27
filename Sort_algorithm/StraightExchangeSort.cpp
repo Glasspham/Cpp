d9613c9f304c2a4bfdc96265fefb6f2606bd7d95
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-27 22:00:25 +0700

    Add files via upload

---
+++ Sort_algorithm/StraightExchangeSort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void straightexchangeSort(int a[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+        for(int j = i + 1; j < n; j++)
+            if(a[i] > a[j])
+                swap(a[i], a[j]);
+}
+
+int main()
+{
+    int a[] = {10, 5, 1, 6, 2, 7, 8, 3, 4, 9};
+    int n = sizeof(a)/sizeof(a[0]);
+    straightexchangeSort(a, n);
+    for(int i = 0; i < n; i++)
+    {
+        cout << a[i] << ' ';
+    }
+}
