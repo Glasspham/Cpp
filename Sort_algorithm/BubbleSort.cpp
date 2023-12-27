9b87c367b7458eef929d87429da0232125dc13db
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-27 22:01:47 +0700

    Update BubbleSort.cpp

--- Sort_algorithm/BubbleSort.cpp
+++ Sort_algorithm/BubbleSort.cpp
@@ -5,17 +5,13 @@ using namespace std;
 void bubbleSort(int a[], int n)
 {
     for(int i = 0; i < n - 1; i++)
-    {
         for(int j = 0; j < n - i - 1; j++)
-        {
             if(a[j] > a[j + 1])
             {
                 int tmp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1]= tmp;
             }
-        }
-    }
 }
 
 int main()
@@ -24,7 +20,5 @@ int main()
     int n = sizeof(a)/sizeof(a[0]);
     bubbleSort(a, n);
     for(int i = 0; i < n; i++)
-    {
         cout << a[i] << ' ';
-    }
 }
