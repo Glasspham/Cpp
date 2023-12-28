3041935704ac366143786e70de40d109fdb90342
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-28 09:02:17 +0700

    Update Binary_tree.cpp

--- Binary_tree.cpp
+++ Binary_tree.cpp
@@ -8,143 +8,144 @@ typedef struct Node
     Node* right;
 } node_t;
 
-
-
-void Free( node_t* root )
+void Free(node_t* root )
 {
-    if ( root == NULL )
+    if(root == NULL)
         return;
 
-    Free( root->left );
-    Free( root->right );
-    free( root );
+    Free(root -> left);
+    Free(root -> right);
+    free(root);
 }
 
-int LeftOf( const int value, const node_t* root )
+int LeftOf(const int value, const node_t* root)
 {
     // Nếu bạn muốn cây BST cho phép giá trị trùng lặp, hãy sử dụng dòng code thứ 2
-    return value < root->data;
-//    return value <= root->data;
+    return value < root -> data;
+    // return value <= root->data;
 }
 
-int RightOf( const int value, const node_t* root )
+int RightOf(const int value, const node_t* root)
 {
-    return value > root->data;
+    return value > root -> data;
 }
 
-node_t* Insert( node_t* root, const int value )
+node_t* Insert(node_t* root, const int value)
 {
-    if ( root == NULL )
+    if(root == NULL)
     {
-        node_t* node = (node_t*)malloc( sizeof( node_t ) );
-        node->left = NULL;
-        node->right = NULL;
-        node->data = value;
+        node_t* node = (node_t*)malloc(sizeof(node_t));
+        node -> left = NULL;
+        node -> right = NULL;
+        node -> data = value;
         return node;
     }
-    if ( LeftOf( value, root ) )
-        root->left = Insert( root->left, value );
-    else if ( RightOf( value, root ) )
-        root->right = Insert( root->right, value );
+    if(LeftOf(value, root))
+        root -> left = Insert(root -> left, value);
+    else if(RightOf(value, root))
+        root -> right = Insert(root -> right, value);
     return root;
 }
 
-bool Search( const node_t* root, int value )
+bool Search(const node_t* root, int value)
 {
-    if ( root == NULL )
+    if(root == NULL)
         return false;
-    if(root->data == value){
+    if(root -> data == value)
         return true;
-    }else if ( LeftOf( value, root ) ){
-        return Search( root->left, value );
-    }else if( RightOf( value, root ) ){
-        return Search( root->right, value );
-    }
+    else if(LeftOf(value, root))
+        return Search(root -> left, value);
+    else if(RightOf(value, root))
+        return Search(root -> right, value);
 }
 
-int LeftMostValue( const node_t* root )
+int LeftMostValue(const node_t* root)
 {
-    while ( root->left != NULL )
-        root = root->left;
-    return root->data;
+    while(root -> left != NULL)
+        root = root -> left;
+    return root -> data;
 }
 
-node_t* Delete( node_t* root, int value )
+node_t* Delete(node_t* root, int value)
 {
-    if ( root == NULL )
+    if(root == NULL)
         return root;
-    if ( LeftOf( value, root ) )
-        root->left = Delete( root->left, value );
-    else if ( RightOf( value, root ) )
-        root->right = Delete( root->right, value );
+    if(LeftOf(value, root))
+        root -> left = Delete(root -> left, value);
+    else if(RightOf(value, root))
+        root -> right = Delete(root -> right, value);
     else
     {
         // root->data == value, delete this node
-        if ( root->left == NULL )
+        if(root -> left == NULL)
         {
-            node_t* newRoot = root->right;
-            free( root );
+            node_t* newRoot = root -> right;
+            free(root);
             return newRoot;
         }
-        if ( root->right == NULL )
+
+        if(root -> right == NULL)
         {
-            node_t* newRoot = root->left;
-            free( root );
+            node_t* newRoot = root -> left;
+            free(root);
             return newRoot;
         }
-        root->data = LeftMostValue( root->right );
-        root->right = Delete( root->right, root->data );
+
+        root -> data = LeftMostValue(root -> right);
+        root -> right = Delete(root -> right, root -> data);
     }
     return root;
 }
 
-void PreOrder(node_t* root){
+void PreOrder(node_t* root)
+{
     if(root != NULL)
     {
-        printf("%d ", root->data);
-        PreOrder(root->left);
-        PreOrder(root->right);
+        printf("%d ", root -> data);
+        PreOrder(root -> left);
+        PreOrder(root -> right);
     }
 }
 
-void InOrder(node_t* root){
+void InOrder(node_t* root)
+{
     if(root != NULL)
     {
-        InOrder(root->left);
-        printf("%d ", root->data);
-        InOrder(root->right);
+        InOrder(root -> left);
+        printf("%d ", root -> data);
+        InOrder(root -> right);
     }
 }
 
-void PostOrder(node_t* root){
+void PostOrder(node_t* root)
+{
     if(root != NULL)
     {
-        PostOrder(root->left);
-        PostOrder(root->right);
-        printf("%d ", root->data);
+        PostOrder(root -> left);
+        PostOrder(root -> right);
+        printf("%d ", root -> data);
     }
 }
-
 
 int main()
 {
     node_t* root = NULL;
 
-    root = Insert( root, 25 );
-    root = Insert( root, 15 );
-    root = Insert( root, 50 );
-    root = Insert( root, 10 );
-    root = Insert( root, 22 );
-    root = Insert( root, 35 );
-    root = Insert( root, 70 );
-    root = Insert( root, 4 );
-    root = Insert( root, 12 );
-    root = Insert( root, 18 );
-    root = Insert( root, 24 );
-    root = Insert( root, 31 );
-    root = Insert( root, 44 );
-    root = Insert( root, 66 );
-    root = Insert( root, 90 );
+    root = Insert(root, 25);
+    root = Insert(root, 15);
+    root = Insert(root, 50);
+    root = Insert(root, 10);
+    root = Insert(root, 22);
+    root = Insert(root, 35);
+    root = Insert(root, 70);
+    root = Insert(root, 4);
+    root = Insert(root, 12);
+    root = Insert(root, 18);
+    root = Insert(root, 24);
+    root = Insert(root, 31);
+    root = Insert(root, 44);
+    root = Insert(root, 66);
+    root = Insert(root, 90);
     printf("\nDuyet preorder : ");
     PreOrder(root);
     printf("\nDuyet inorder  : ");
@@ -161,7 +162,6 @@ int main()
     printf("\nDuyet postorder:");
     PostOrder(root);
 
-
     printf("\n==Thu xoa phan tu 50 khoi BTS==\n");
     Delete(root, 50);
     printf("\nDuyet preorder : ");
@@ -171,10 +171,7 @@ int main()
     printf("\nDuyet postorder:");
     PostOrder(root);
 
-
-
-
-    Free( root );
+    Free(root);
     root = NULL;
     return 0;
 }
