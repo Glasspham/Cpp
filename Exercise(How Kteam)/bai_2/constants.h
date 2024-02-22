cbd92e3de001f96b31c21b3ca0d7cf536d217a08
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-02-22 16:35:39 +0700

    Update constants.h

--- Exercise(How Kteam)/bai_2/constants.h
+++ Exercise(How Kteam)/bai_2/constants.h
@@ -9,9 +9,9 @@ using namespace std;
 // Định nghĩa namespace chứa các hằng số
 namespace constants
 {
-	const int nguong_1(1);
-	const int nguong_2(5);
-	const int nguong_3(120);
+    const int nguong_1(1);
+    const int nguong_2(5);
+    const int nguong_3(120);
     const int gia_1(15000);
     const int gia_2(13500);
     const int gia_3(11000);
