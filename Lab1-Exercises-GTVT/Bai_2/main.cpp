8bb61a14defb01582757831c841cea6f555b7fd9
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-02-22 21:31:17 +0700

    Delete Lab1-Exercises-GTVT directory

--- Lab1-Exercises-GTVT/Bai_2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a, b;
-    cout << "Nhap vao 2 do dai hinh chu nhat: ";
-    cin >> a >> b;
-    cout << "Chu vi hinh hinh chu nhat: " << 2 * (a + b) << endl;
-}
--- Lab1-Exercises-GTVT/Bai_5/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    double math, physics, chemistry;
-    cout << "Nhap diem mon toan ly hoa: ";
-    cin >> math >> physics >> chemistry;
-    cout << "Diem trung binh cong: " << (math + physics + chemistry) / 3 << endl;
-}
