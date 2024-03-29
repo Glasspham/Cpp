4f106cbe0f843106a0ecfcbdacf8a454367cef48
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-03-29 17:25:19 +0700

    Create School_year_corresponding_to_class.cpp

---
+++ School_year_corresponding_to_class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void year(int &j){
+	for(int i = 1; j < j + 18; i++, j++){
+		if(i < 13)
+			cout << "Lop " << i << " : " << j << '-' << j + 1 << endl;
+		else if( i == 13)
+			cout << "Sinh vien nam 1 : " << j << '-' << j + 1 << endl;
+		else if( i == 14)
+			cout << "Sinh vien nam 2 : " << j << '-' << j + 1 << endl;
+		else if( i == 15)
+			cout << "Sinh vien nam 3 : " << j << '-' << j + 1 << endl;
+		else if( i == 16)
+			cout << "Sinh vien nam 4 : " << j << '-' << j + 1 << endl;
+		else
+			break;
+	}
+}
+
+int main(){
+	cout << "Nam sinh cua ban la: ";
+	int j;
+	cin >> j;
+	j += 6;
+	year(j);
+	system("pause");
+	return 0;
+}
