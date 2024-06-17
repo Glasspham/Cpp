b674bf32f67bab0914bc902fe0feb6eff0dee81b
Glasspham <hathimongthu1980@gmail.com>
2024-06-17 19:34:38 +0700

    Fix

--- Binary/Binary_tree.cpp
+++ Binary/Binary_tree.cpp
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
-{
+typedef struct Node{
     int data;
     Node* left;
     Node* right;
 } node_t;
 
-void Free(node_t* root)
-{
+void Free(node_t* root){
     if(root == NULL)
         return;
 
@@ -18,22 +16,18 @@ void Free(node_t* root)
     free(root);
 }
 
-int LeftOf(const int value, const node_t* root)
-{
+int LeftOf(const int value, const node_t* root){
     // Nếu bạn muốn cây BST cho phép giá trị trùng lặp, hãy sử dụng dòng code thứ 2
     return value < root -> data;
     // return value <= root->data;
 }
 
-int RightOf(const int value, const node_t* root)
-{
+int RightOf(const int value, const node_t* root){
     return value > root -> data;
 }
 
-node_t* Insert(node_t* root, const int value)
-{
-    if(root == NULL)
-    {
+node_t* Insert(node_t* root, const int value){
+    if(root == NULL){
         node_t* node = (node_t*)malloc(sizeof(node_t));
         node -> left = NULL;
         node -> right = NULL;
@@ -47,8 +41,7 @@ node_t* Insert(node_t* root, const int value)
     return root;
 }
 
-bool Search(const node_t* root, int value)
-{
+bool Search(const node_t* root, int value){
     if(root == NULL)
         return false;
     if(root -> data == value)
@@ -59,33 +52,28 @@ bool Search(const node_t* root, int value)
         return Search(root -> right, value);
 }
 
-int LeftMostValue(const node_t* root)
-{
+int LeftMostValue(const node_t* root){
     while(root -> left != NULL)
         root = root -> left;
     return root -> data;
 }
 
-node_t* Delete(node_t* root, int value)
-{
+node_t* Delete(node_t* root, int value){
     if(root == NULL)
         return root;
     if(LeftOf(value, root))
         root -> left = Delete(root -> left, value);
     else if(RightOf(value, root))
         root -> right = Delete(root -> right, value);
-    else
-    {
+    else{
         // root->data == value, delete this node
-        if(root -> left == NULL)
-        {
+        if(root -> left == NULL){
             node_t* newRoot = root -> right;
             free(root);
             return newRoot;
         }
 
-        if(root -> right == NULL)
-        {
+        if(root -> right == NULL){
             node_t* newRoot = root -> left;
             free(root);
             return newRoot;
@@ -97,38 +85,31 @@ node_t* Delete(node_t* root, int value)
     return root;
 }
 
-void PreOrder(node_t* root)
-{
-    if(root != NULL)
-    {
+void PreOrder(node_t* root){
+    if(root != NULL){
         printf("%d ", root -> data);
         PreOrder(root -> left);
         PreOrder(root -> right);
     }
 }
 
-void InOrder(node_t* root)
-{
-    if(root != NULL)
-    {
+void InOrder(node_t* root){
+    if(root != NULL){
         InOrder(root -> left);
         printf("%d ", root -> data);
         InOrder(root -> right);
     }
 }
 
-void PostOrder(node_t* root)
-{
-    if(root != NULL)
-    {
+void PostOrder(node_t* root){
+    if(root != NULL){
         PostOrder(root -> left);
         PostOrder(root -> right);
         printf("%d ", root -> data);
     }
 }
 
-int main()
-{
+int main(){
     node_t* root = NULL;
 
     root = Insert(root, 25);
